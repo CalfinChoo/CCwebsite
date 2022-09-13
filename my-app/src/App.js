fa4dfe8c80fac14f1969784c1d716010434a3446
Calvin Chu <cchu00@stuy.edu>
2022-09-13 15:48:51 -0400

    start

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="bg-dark-purple w-screen h-screen">
+      <h1 className="py-40 text-3xl text-white font-bold underline">
+        Hello
+      </h1>
+
+    </div>
+  );
+}
+
+export default App;
